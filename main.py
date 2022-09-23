7e4b6de77a4bc559c62233a701e359a2edca851c
kim.s <kim.s@suncreature.com>
2022-09-23 12:16:30 +0200

    testSetup

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import os, sys
+path = os.path.abspath((os.path.dirname(__file__)))
+
+if path not in sys.path:
+    print('Adding path to PythonPath:', path)
+    sys.path.append(path)
+
 from sg_py import hello_world
 
-if __name__ == '__main__':
+def run():
     hello_world.hello_world()
+
+if __name__ == '__main__':
+    run()
